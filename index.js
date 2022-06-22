5cbea9d286cac3bff9a11f2d2e082352370743e7
Diego Barboza <diegojose1994@gmail.com>
2022-06-22 13:02:36 -0400

    Test Date 5

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ setInterval(initializeScrape, 1000);
 telegramBot()
 
 
+const systemDate = moment().utcOffset() 
+const now = moment().utcOffset(systemDate).format('hh:mm:ss A');
+
+console.log(`Sistema iniciado`)
+console.log(`UTC ${systemDate}`)
+console.log(`Now ${now}`)
 
 
 
