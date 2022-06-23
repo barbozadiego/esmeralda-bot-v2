b7efdc11a73f9431efca6c690488b3d8c5206e03
Diego Barboza <diegojose1994@gmail.com>
2022-06-22 21:42:04 -0400

    Update dollarBCV.js

--- src/dollarBCV.js
+++ src/dollarBCV.js
@@ -8,8 +8,8 @@ import { addDollarBCV } from './firebase/firebaseConfig.js';
 const queryHours = {
     "eightMorning": moment('08:00 AM','hh:mm A').format('hh:mm A'),
     "nineMorning": moment('09:03 AM','hh:mm A').format('hh:mm A'),
-    "oneAfternoon": moment('09:34 PM','hh:mm A').format('hh:mm A'),
-    "fiveAfternoon": moment('09:40 PM','hh:mm A').format('hh:mm A')
+    "oneAfternoon": moment('09:52 PM','hh:mm A').format('hh:mm A'),
+    "fiveAfternoon": moment('09:47 PM','hh:mm A').format('hh:mm A')
 };
 
 const getPriceDollarBCV = async () => {
